1b6b67d748fd59c1bd8e5a5c9a4adf1cb4f58b91
Sagi Chubok <49566568+SagiChubok@users.noreply.github.com>
2020-12-23 23:31:34 +0200

    Add logger

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const logger = require("morgan");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
