3464d5af786f291fe33008bb6faaaea5a719a18e
Sagi Chubok <49566568+SagiChubok@users.noreply.github.com>
2020-12-23 23:29:40 +0200

    Set-up server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something is broken!');
+});
+
+app.listen(port, () => console.log('Express server is running on port ', port));
