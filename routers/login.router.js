43b490f639cf10273980a85bcb9f3ebe79540f43
LinoyChubok <linoychubok@gmail.com>
2020-12-24 14:00:06 +0200

    Add auth middleware

--- routers/login.router.js
+++ routers/login.router.js
@@ -1,7 +1,8 @@
 const { Router } = require ('express');
 const { loginController } = require ('../controllers/login.ctrl');
+const { ensureAuth, ensureGuest } = require('../middlewares/auth')
 const loginRouter = new Router();
 
-loginRouter.get('/', loginController.login);
+loginRouter.get('/', ensureAuth, loginController.login);
 
 module.exports = { loginRouter };
