dbe2568f200a4a8114aaaad0734537053e49c380
Sagi Chubok <49566568+SagiChubok@users.noreply.github.com>
2020-12-24 10:48:58 +0200

    Update constants

--- config/constants.js
+++ config/constants.js
@@ -2,4 +2,6 @@ module.exports = {
     DB_HOST: process.env.DB_HOST,
     DB_USER: process.env.DB_USER,
     DB_PASS: process.env.DB_PASS,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
 }
