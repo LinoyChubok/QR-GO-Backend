ed3ee025afd35275fb0da0f6135828223781b1f0
LinoyChubok <linoychubok@gmail.com>
2020-12-29 00:33:39 +0200

    Create QR URL with crypto string

--- api/controllers/qr.ctrl.js
+++ api/controllers/qr.ctrl.js
@@ -1,9 +1,14 @@
+const cryptoRandomString = require('crypto-random-string');
+ 
 exports.qrController = {
     
     getQR(req, res) {
         res.send("ok");
     },
     createQR(req, res) {
-        res.send("ok");
+        const randomString = cryptoRandomString({length: 30, type: 'alphanumeric'});
+        const service_url = "http://localhost:3000/api/qr/";
+        const qrURL = `${service_url}${randomString}`;
+        res.send(qrURL);
     }  
 }
