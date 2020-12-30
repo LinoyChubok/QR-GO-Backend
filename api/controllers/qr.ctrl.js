417110cb64b5414f9074c1d342a01fbd2cfc1d14
Sagi Chubok <49566568+SagiChubok@users.noreply.github.com>
2020-12-30 15:10:45 +0200

    Create QR - Change from data to secretkey

--- api/controllers/qr.ctrl.js
+++ api/controllers/qr.ctrl.js
@@ -8,14 +8,14 @@ exports.qrController = {
     createQR(req, res) {
         const randomString = cryptoRandomString({length: 30, type: 'alphanumeric'});
         const service_url = "http://localhost:3000/api/qr/";
-        const qr_data = `${service_url}${randomString}`;
+        const qr_secretkey = `${service_url}${randomString}`;
         
-        const qr_url = `https://api.qrserver.com/v1/create-qr-code/?size=700x700&data=${qr_data}`;
+        const qr_url = `https://api.qrserver.com/v1/create-qr-code/?size=700x700&data=${qr_secretkey}`;
         axios.get(qr_url, {responseType: 'arraybuffer'})
           .then(response => {
             res.status(200).json({
-                qrdata: qr_data,
-                qrurl: Buffer.from(response.data, 'binary').toString('base64')
+                secretkey: qr_secretkey,
+                url: Buffer.from(response.data, 'binary').toString('base64')
             })
         }).catch(error => {
             res.status(500).json({
