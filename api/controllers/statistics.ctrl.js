c2f3eb712581c0a069bf672885c9422029a6bb1c
Sagi Chubok <49566568+SagiChubok@users.noreply.github.com>
2021-02-23 13:59:01 +0200

    Handle stats calc

--- api/controllers/statistics.ctrl.js
+++ api/controllers/statistics.ctrl.js
@@ -1,10 +1,109 @@
-const Game = require('../models/game.model');
-const Route = require('../models/route.model');
+const Group = require('../models/group.model');
 
 exports.statisticsController = {
-    getStatistics(req, res) {
+    async getStatistics(req, res) {
         const gameId = req.params.id;
-        console.log(gameId);
-        res.send("work");
+
+        try {
+            let groups = await Group.find({
+                'game': gameId
+            }).populate({
+                path: 'game',
+                populate: {
+                    path: 'route'
+                }
+            });
+
+            const game = groups[0].game;
+
+            let i, j;
+            const groupsLength = groups.length;
+            const challengesAmount = game.route.challengesAmount;
+
+            let timeArr = [];
+            for (i = 0; i < groupsLength; i++) {
+
+                const groupChallenges = groups[i].challengesTime.length;
+
+                timeArr.push(groups[i].groupName);
+                if (groupChallenges == 1) {
+                    let challengeTimeMinutesCurrent = groups[i].challengesTime[0].getHours() * 60 + groups[i].challengesTime[0].getMinutes();
+                    let createdAt = game.createdAt.getHours() * 60 + game.createdAt.getMinutes();
+                    timeArr.push(challengeTimeMinutesCurrent - createdAt);
+
+                    let j = 1;
+                    while (j < challengesAmount) {
+                        timeArr.push(0);
+                        j++;
+                    }
+                } else {
+                    for (j = 0; j < groupChallenges - 1; j++) {
+
+                        if (j == 0) {
+                            let challengeTimeMinutesCurrent = groups[i].challengesTime[j].getHours() * 60 + groups[i].challengesTime[j].getMinutes();
+                            let createdAt = game.createdAt.getHours() * 60 + game.createdAt.getMinutes();
+                            timeArr.push(challengeTimeMinutesCurrent - createdAt);
+
+                        } else {
+                            let challengeTimeMinutesCurrent = groups[i].challengesTime[j].getHours() * 60 + groups[i].challengesTime[j].getMinutes();
+                            let challengeTimeMinutesNext = groups[i].challengesTime[j + 1].getHours() * 60 + groups[i].challengesTime[j + 1].getMinutes();
+                            timeArr.push(challengeTimeMinutesNext - challengeTimeMinutesCurrent);
+                        }
+
+                    }
+                    j++;
+                    while (j < challengesAmount) {
+                        timeArr.push(0);
+                        j++;
+                    }
+                }
+            }
+
+            let dataGame = [];
+            let challengeTimes = []
+
+            // Insert Challenges
+            for (i = 0; i < challengesAmount; i++) {
+                dataGame.push(`{"name":"Challenge ${i + 1}"`)
+            }
+
+            // Insert game time for each group in each challenge
+            let countGroup = 0;
+            for (i = 0; i < timeArr.length; i++) {
+                if (typeof timeArr[i] === 'string') {
+                    ++countGroup;
+                }
+                if (typeof timeArr[i] !== 'string') {
+                    for (j = 0; j < challengesAmount; j++) {
+                        let timePerGroup = `"Group_${j + 1}": ${timeArr[i + j]}`
+                        dataGame[countGroup - 1] = `${dataGame[countGroup - 1]}, ${timePerGroup}`
+                        if (j === challengesAmount - 1) {
+                            dataGame[countGroup - 1] = `${dataGame[countGroup - 1]} }`
+                        }
+                    }
+                    if (countGroup !== groupsLength)
+                        i = i + challengesAmount - 1;
+                    else
+                        break;
+                }
+            }
+
+            let gameData = [];
+            let a = dataGame.map(
+                challenge => {
+                    gameData.push(JSON.parse(challenge));
+                    return challenge;
+                }
+            )
+            console.log(gameData)
+
+            res.status(200).json({
+                gameData
+            })
+
+        } catch (e) {
+            console.log(e);
+        }
+
     },
 }
